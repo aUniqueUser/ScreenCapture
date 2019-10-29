e26689d4f95d15bce20d42241a10f8710de9a892
aUniqueUser <55301516+aUniqueUser@users.noreply.github.com>
2019-10-29 21:51:25 +0000

    Formatting

--- src/capture.py
+++ src/capture.py
@@ -6,10 +6,8 @@ import time
 lastTime = time.time()
 while True:
     capture = np.array(ImageGrab.grab(bbox=(0, 40, 500, 500)))  # Change these values to change the capture size
-
     print('Capture took {} seconds'.format(time.time()-lastTime))
     lastTime = time.time()
-
     cv2.imshow('Capture', cv2.cvtColor(capture, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('x'):
         cv2.destroyAllWindows()
